719730efd5fe14239cba4f425b98ae1d9fd9408e
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-06-13 22:51:02 +0000

    draw stuff

--- code.py
+++ code.py
@@ -1,13 +1,11 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: Copyright 2025 Sam Blenny
-#
-# Fruit Jam Spirals
-#
 from board import CKP, CKN, D0P, D0N, D1P, D1N, D2P, D2N
 import displayio
 from displayio import Bitmap, Group, Palette, TileGrid
 import framebufferio
 import gc
+import math
 import picodvi
 import supervisor
 from time import sleep
@@ -34,11 +32,51 @@ grp = Group(scale=1)
 display.root_group = grp
 
 # Make bitmap + palette + tilegrid so we have a canvas to draw on
-bitmap = Bitmap(width, height, 256)
-palette = Palette(256)
-for i in range(len(palette)):
-    palette[i] = i
+palette_size = 1 << color_depth
+bitmap = Bitmap(width, height, palette_size)
+palette = Palette(palette_size)
+for i in range(palette_size):
+    # Expand 8-bit RGB332 (i) into a 24-bit RGB888 tuple (r, g, b)
+    r = i & 0xE0
+    g = (i & 0x1C) << 3
+    b = (i & 0x03) << 6
+    palette[i] = (r, g, b)
 tilegrid = TileGrid(bitmap, pixel_shader=palette)
 grp.append(tilegrid)
 
-# TODO: DRAW SPIRALS
+def draw_spiral(a, b, h, cycles=64, oversample=2, delay_ms=1, end_pause_s=3):
+    """Draw a Hypotrochoid spiral
+    a: outer circle radius in pixels
+    b: inner circle radius in pixels
+    h: drawing pen radius in pixels from center of inner circle
+    cycles: how many 360 degree cycles to run the equations through
+    oversample: increase this to make the pixel density higher
+    delay_ms: increase this to draw slower (unit: ms)
+    end_pause_s: increase this to pause for longer at the end (unit: seconds)
+    """
+    global bitmap
+    global display
+    rad = math.radians
+    sin = math.sin
+    cos = math.cos
+    x0 = width / 2
+    y0 = height / 2
+    bitmap.fill(0)
+    for t in range(cycles * 360 * oversample):
+        color = (t >> 1) & 0xff  # this cycles through all the palette colors
+        tr = rad(t/oversample)
+        x = round(x0 + (a - b) * cos(tr) + h * cos((a - b) / b * tr))
+        y = round(y0 + (a - b) * sin(tr) - h * sin((a - b) / b * tr))
+        if (0 <= x < width) and (0 <= y < height):
+            bitmap[x, y] = color
+            display.refresh()
+        sleep(0.001 * delay_ms)
+    # Wait at the end so you can look at the final image for a bit
+    sleep(end_pause_s)
+
+# Sleep after drawing is done to prevent supervisor from erasing display
+while True:
+    draw_spiral(a=110, b=12, h=11, cycles=6, delay_ms=3)
+    draw_spiral(a=160, b=80, h=10, cycles=1, delay_ms=3)
+    draw_spiral(a=111, b=25, h=9, cycles=6, delay_ms=3)
+    draw_spiral(a=111, b=25, h=29, cycles=27)
